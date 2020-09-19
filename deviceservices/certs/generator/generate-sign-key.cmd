730fca1d5c639dad486f393a131e7cf8730639c6
CrEye1 <63877076+CrEye1@users.noreply.github.com>
2020-09-19 00:16:02 -0400

    Update generate-sign-key.cmd

--- deviceservices/certs/generator/generate-sign-key.cmd
+++ deviceservices/certs/generator/generate-sign-key.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-SET OPENSSL_CONF=C:\OpenServer\modules\http\Apache-2.4\conf\openssl.cnf
-PATH=%PATH%;C:\OpenServer\modules\http\Apache-2.4\bin
+SET OPENSSL_CONF=C:\xampp\apache\openssl.cnf
+PATH=%PATH%;C:\xampp\apache\bin
 openssl genrsa -out signature_private.key 1024
 rem openssl rsa -in signature_private.key -pubout -outform PEM -out signature_public.key
 pause
