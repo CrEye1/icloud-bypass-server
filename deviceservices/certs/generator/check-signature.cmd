a5ad8597720f49dee882c8d28914a9df953cffa9
CrEye1 <63877076+CrEye1@users.noreply.github.com>
2020-09-19 00:12:33 -0400

    Update check-signature.cmd

--- deviceservices/certs/generator/check-signature.cmd
+++ deviceservices/certs/generator/check-signature.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-SET OPENSSL_CONF=C:\OpenServer\modules\http\Apache-2.4\conf\openssl.cnf
-PATH=%PATH%;C:\OpenServer\modules\http\Apache-2.4\bin
+SET OPENSSL_CONF=C:\xampp\apache\conf\openssl.cnf
+PATH=%PATH%;C:\xampp\apache\bin
 rem openssl rsautl -sign -in accounttoken.txt -out signature.txt -inkey signature_private.key
 openssl rsautl -verify -in _signature.txt -out _accounttoken.txt -inkey ../signature_public.key -pubin
 
