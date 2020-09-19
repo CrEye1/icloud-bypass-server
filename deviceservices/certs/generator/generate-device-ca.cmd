6909a9eacabdd5659ecaa96275af9839ebac9c12
CrEye1 <63877076+CrEye1@users.noreply.github.com>
2020-09-19 00:13:45 -0400

    Update generate-device-ca.cmd

--- deviceservices/certs/generator/generate-device-ca.cmd
+++ deviceservices/certs/generator/generate-device-ca.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-SET OPENSSL_CONF=C:\OpenServer\modules\http\Apache-2.4\conf\openssl.cnf
-PATH=%PATH%;C:\OpenServer\modules\http\Apache-2.4\bin
+SET OPENSSL_CONF=C:\xampp\apache\conf\openssl.cnf
+PATH=%PATH%;C:\xampp\apache\bin
 REM iPhone Device CA
 openssl req -new -x509 -keyout iPhoneDeviceCA_private.key -out iPhoneDeviceCA.crt -days 3653 -set_serial 0x01 -config extensions_device_ca.cnf -reqexts v3_req -extensions v3_req -passout pass:icloud
 rem openssl x509 -x509toreq -in iPhoneDeviceCA.crt -out iPhoneDeviceCA.csr -signkey iPhoneDeviceCA_private.key -passin pass:icloud
